8689bed1ce7e7375ed3b560758b63017647b149b
Amir <amir.mo.ahmadi@gmail.com>
2026-05-21 10:41:37 +0330

    whay design patterns?

---
+++ DesighPatterns/step1.py
@@ -0,0 +1,32 @@
+#what is Design pattern ?
+# Design patterns are proven, reusable solutions for common problems in software design,
+# providing templates or guidelines for structuring code to solve recurring challenges in a
+# consistent.
+
+
+#Whay Desing Pattens?
+#1. Reusable solutions
+#2. Standardized Terminology
+# Scalability
+# Maintainablility
+# Profrmance
+# Documentation
+# Best practives
+# Cross-Domain Applicability
+
+#Gang of four(GOF) Design patterns (1990)
+# Creational: the different ways to create objects.
+# Structural the relationship between those objects.
+# Behavioral: the interaction or communication between those objects.
+
+# - Behavioural:
+#  - state pattern
+#  - observer pattern
+
+# - Structural
+#  - Facade pattern
+#  - Adapter pattern
+#
+# - Creational
+#  - Prototype Pattern
+#  - Abstract factory pattern
