4f376b56be8d19cb4e256539bb8d5a00686c65ba
Amir <amir.mo.ahmadi@gmail.com>
2026-05-21 10:42:18 +0330

    whay design patterns?

---
+++ DesighPatterns/step2_BehavioralDP.py
@@ -0,0 +1,4 @@
+# Behavioral Design Patterns
+
+# Behavioral Design patterns focus on how objects interact with each other and hwo they communiccate to
+# accomplish specific tasks (such as algorithms and rules(task)).
