fb543021d1069732a40059d4e45a87653cd8be28
Amir <amir.mo.ahmadi@gmail.com>
2026-05-23 11:29:26 +0330

    Facade provides a simplified interface to a complex subsystem.

---
+++ DesighPatterns/step6_SDP_introdation.py
@@ -0,0 +1,10 @@
+#Structural design patterns
+
+# structural design pattern focus on
+# the composition of classed and objects to form larger structures and systems
+
+#structural design patterns help to
+# achieve several important goals in software development:
+# - promote code reusability and modularity
+# - Enhance flexibility and extensibility
+# - Improve performance and resource utilization
